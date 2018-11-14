7303795aa3bd9a49e0a4d5cc9afdf3392142e3d4
Michael Edlund <medlund@mac.com>
2018-11-14 10:55:01 -0800

    Comment out field test specs for now

--- spec/models_spec.rb
+++ spec/models_spec.rb
@@ -9,12 +9,12 @@ RSpec.describe "Data classes" do
   end
 end
 
-Reso::DataDictionary.specification.resources.each do |resource|
-  RSpec.describe "#{resource} attributes" do
-    Reso::DataDictionary.specification.fields_for_class(resource).map{|f| f[:attribute_name]}.each do |attr|
-      it attr do
-      end
-    end
-  end
-end
+# Reso::DataDictionary.specification.resources.each do |resource|
+#   RSpec.describe "#{resource} attributes" do
+#     Reso::DataDictionary.specification.fields_for_class(resource).map{|f| f[:attribute_name]}.each do |attr|
+#       it attr do
+#       end
+#     end
+#   end
+# end
 
