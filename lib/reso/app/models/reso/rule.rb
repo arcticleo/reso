929af046b9bf3c83fa0f59f535aaaac12eaea818
Michael Edlund <medlund@mac.com>
2020-01-15 11:05:09 -0800

    RESO::Rule stubs

---
+++ lib/reso/app/models/reso/rule.rb
@@ -0,0 +1,85 @@
+class RESO::Rule
+
+  def class_name
+  end
+
+  def field_key
+  end
+
+  def field_key_numeric
+  end
+
+  def field_name
+  end
+
+  def modification_timestamp
+  end
+
+  def original_entry_timestamp
+  end
+
+  def originating_system_id
+  end
+
+  def originating_system_name
+  end
+
+  def originating_system_rule_key
+  end
+
+  def resource_name
+  end
+
+  def rule_action
+  end
+
+  def rule_description
+  end
+
+  def rule_enabled_yn
+  end
+
+  def rule_error_text
+  end
+
+  def rule_expression
+  end
+
+  def rule_format
+  end
+
+  def rule_help_text
+  end
+
+  def rule_key
+  end
+
+  def rule_key_numeric
+  end
+
+  def rule_name
+  end
+
+  def rule_order
+  end
+
+  def rule_type
+  end
+
+  def rule_version
+  end
+
+  def rule_warning_text
+  end
+
+  def source_system_history_key
+  end
+
+  def source_system_id
+  end
+
+  def source_system_name
+  end
+
+end
+
