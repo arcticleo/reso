e5c8505567e7b929b51561cdfa836653fa3aebca
Michael Edlund <medlund@mac.com>
2020-01-21 14:10:54 -0800

    has_one RESO::Lookup::OwnershipType

--- lib/reso/app/models/reso/lookup/ownership_type.rb
+++ lib/reso/app/models/reso/lookup/ownership_type.rb
@@ -1,4 +1,4 @@
 class RESO::Lookup::OwnershipType < RESO::Enumeration
-  has_many :ownership_type_assignments, foreign_key: :enumeration_id
-  has_many :reso_property_businesses, through: :ownership_type_assignments, source: :enumerable, source_type: "RESO::Property::Business"
+  has_one :ownership_type_assignment, foreign_key: :enumeration_id
+  has_one :reso_property_businesses, through: :ownership_type_assignment, source: :enumerable, source_type: "RESO::Property::Business"
 end
